4f65726b754bd9c9b789fdbf53edf1d27c06deb0
Tyler <t.angley@protonmail.com>
2022-03-08 08:50:31 -0500

    update version

--- src/workspace/Get.lua
+++ src/workspace/Get.lua
@@ -7,7 +7,7 @@ local Modules = ReplicatedStorage:WaitForChild("Modules")
 local Get = {
 	-- The latest version can be found here:
 	-- https://github.com/namatchi/gamebuilder/blob/main/src/workspace/Get.lua
-	Version = "1.1.5",
+	Version = "1.1.6",
 
 	-- Used to indicate members of a directory in Get searches
 	Directory = ".",
