38a9671f29943a79e8756be3d085525642da404d
Tyler <t.angley@protonmail.com>
2022-05-18 03:52:03 -0400

    add remodel workflow

---
+++ .remodel/export.lua
@@ -0,0 +1,22 @@
+-- Compiles models from models.rbxlx to the instance tree.
+local game = remodel.readPlaceFile("sources.rbxlx")
+
+-- get source folders
+local exports = {
+	["temp/models/gui/"] = game:GetService("StarterGui"),
+	["temp/models/assets/"] = game:GetService("Workspace").Assets,
+	["temp/models/storage/"] = game:GetService("ServerStorage"),
+	["temp/models/lighting/"] = game:GetService("Lighting"),
+	["temp/models/map/"] = game:GetService("Workspace").Map,
+}
+
+for directory, instance in pairs (exports) do
+	-- create directory for these files
+	remodel.createDirAll(directory)
+
+	-- export all children into this directory
+	local children = instance:GetChildren()
+	for _, model in pairs (children) do
+		remodel.writeModelFile(model, directory .. model.Name .. ".rbxmx")
+	end
+end
